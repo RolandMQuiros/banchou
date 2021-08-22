a495adfef9e5289318e6e254293cdcc91005fb9f
Roland Quiros <afictitiousforce@gmail.com>
2021-08-22 12:38:50 -0700

    Added PawnRootMotion FSM behavior Separated observable extensions for players to PlayerEvents.cs

---
+++ Assets/Banchou/Code/Pawns/FSM/PawnRootMotion.cs
@@ -0,0 +1,35 @@
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Banchou.Pawn.FSM {
+    public class PawnRootMotion : FSMBehaviour {
+        [SerializeField] private bool _rootPosition = true;
+        [SerializeField] private bool _rootRotation = false;
+        private GameState _state;
+        private PawnSpatial _spatial;
+        
+        public void Construct(GameState state, GetPawnId getPawnId, Animator animator) {
+            _state = state;
+            _state.ObservePawnSpatial(getPawnId())
+                .CatchIgnoreLog()
+                .Subscribe(spatial => {
+                    _spatial = spatial;
+                })
+                .AddTo(this);
+        }
+
+        public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            if (_rootPosition) {
+                _spatial.Move(animator.deltaPosition, _state.GetTime());
+            }
+
+            if (_rootRotation) {
+                _spatial.Rotate(
+                    animator.deltaRotation * _spatial.Forward,
+                    _state.GetTime()
+                );
+            }
+        }
+    }
+}
---
+++ Assets/Banchou/Code/Player/State/PlayerEvents.cs
@@ -0,0 +1,56 @@
+using System;
+using UniRx;
+
+using Banchou.Network;
+
+namespace Banchou.Player {
+    public static class PlayerEvents {
+        public static IObservable<PlayersState> ObservePlayers(this GameState state) {
+            return state.Players.Observe();
+        }
+
+        public static IObservable<PlayerState> ObservePlayer(this GameState state, int playerId) {
+            return state
+                .ObservePlayers()
+                .SelectMany(players => {
+                    PlayerState player;
+                    if (players.Members.TryGetValue(playerId, out player)) {
+                        return player.Observe();
+                    }
+                    return Observable.Empty<PlayerState>();
+                });
+        }
+
+        public static IObservable<PlayerInputState> ObservePlayerInput(this GameState state, int playerId) {
+            return state
+                .ObservePlayer(playerId)
+                .SelectMany(player => player.Input.Observe());
+        }
+
+        public static IObservable<PlayerInputState> ObserveLocalPlayerInput(this GameState state) {
+            return state
+                .ObservePlayers()
+                .SelectMany(players => players.Members.Values)
+                .SelectMany(player => player.Observe())
+                .Where(player => state.IsLocalPlayer(player.PlayerId))
+                .SelectMany(player => player.Input.Observe());
+        }
+
+        public static IObservable<PlayerState> ObserveAddedPlayers(this GameState state) {
+            return Observable
+                .FromEvent<PlayerState>(
+                    h => state.Players.PlayerAdded += h,
+                    h => state.Players.PlayerAdded -= h
+                )
+                .StartWith(state.Players.Members.Values);
+        }
+
+        public static IObservable<PlayerState> ObserveRemovedPlayers(this GameState state) {
+            return Observable
+                .FromEvent<PlayerState>(
+                    h => state.Players.PlayerRemoved += h,
+                    h => state.Players.PlayerRemoved -= h
+                );
+        }
+    }
+}
