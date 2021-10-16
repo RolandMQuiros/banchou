761c6aebdac280525e9d6867977553f3f5da5849
Roland Quiros <afictitiousforce@gmail.com>
2021-10-15 20:55:26 -0700

    Fixed broken script reference. Fixed incorrect timing on PawnRootMotion deltas

--- Assets/Banchou/Code/Pawns/FSM/PawnRootMotion.cs
+++ Assets/Banchou/Code/Pawns/FSM/PawnRootMotion.cs
@@ -7,8 +7,9 @@ namespace Banchou.Pawn.FSM {
         [SerializeField] private bool _rootRotation = false;
         private GameState _state;
         private PawnSpatial _spatial;
+        private Animator _animator;
         
-        public void Construct(GameState state, GetPawnId getPawnId, Animator animator) {
+        public void Construct(GameState state, GetPawnId getPawnId) {
             _state = state;
             _state.ObservePawnSpatial(getPawnId())
                 .CatchIgnoreLog()
@@ -17,12 +18,12 @@ namespace Banchou.Pawn.FSM {
                 })
                 .AddTo(this);
         }
-
-        public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
             if (_rootPosition) {
                 _spatial.Move(animator.deltaPosition, _state.GetTime());
             }
-
+        
             if (_rootRotation) {
                 _spatial.Rotate(
                     animator.deltaRotation * _spatial.Forward,
