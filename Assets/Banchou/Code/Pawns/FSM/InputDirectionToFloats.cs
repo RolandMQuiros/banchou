3632e9a545cfa37c1a5f65713c572ed859e967a1
Roland Quiros <afictitiousforce@gmail.com>
2021-11-06 13:42:25 -0700

    Added Isaac strafe animations. Cleaned up some unused animation files.

---
+++ Assets/Banchou/Code/Pawns/FSM/InputDirectionToFloats.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Banchou.Player;
+using UniRx;
+using UnityEngine;
+
+namespace Banchou.Pawn.FSM {
+    public class InputDirectionToFloats : FSMBehaviour {
+        [SerializeField] private string _magnitudeOut;
+        [SerializeField] private string _forwardOut;
+        [SerializeField] private string _rightOut;
+
+        private PlayerInputState _player;
+        private PawnSpatial _spatial;
+
+        private int _magnitudeHash;
+        private int _forwardHash;
+        private int _rightHash;
+
+        public void Construct(GameState state, GetPawnId getPawnId) {
+            state.ObservePawnInput(getPawnId())
+                .CatchIgnoreLog()
+                .Subscribe(player => _player = player)
+                .AddTo(this);
+            state.ObservePawnSpatial(getPawnId())
+                .CatchIgnoreLog()
+                .Subscribe(spatial => _spatial = spatial)
+                .AddTo(this);
+            
+            _magnitudeHash = Animator.StringToHash(_magnitudeOut);
+            _forwardHash = Animator.StringToHash(_forwardOut);
+            _rightHash = Animator.StringToHash(_rightOut);
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            if (_player == null) return;
+            
+            if (_magnitudeHash != 0) {
+                animator.SetFloat(_magnitudeHash, _player.Direction.magnitude);
+            }
+
+            if (_forwardHash != 0) {
+                animator.SetFloat(_forwardHash, Vector3.Dot(_player.Direction, _spatial.Forward));
+            }
+
+            if (_rightHash != 0) {
+                animator.SetFloat(_rightHash, Vector3.Dot(_player.Direction, _spatial.Right));
+            }
+        }
+    }   
+}
